3bca8fc03c54825457d2cdb9475f3fe9c1dd4b52
Arnav Puri <arnavpuri08@gmail.com>
2025-04-03 13:58:51 +0530

    Admin Added_2.0

--- src/main/java/com/example/ecomm/config/SecurityConfig.java
+++ src/main/java/com/example/ecomm/config/SecurityConfig.java
@@ -14,17 +14,17 @@
 //        return authenticationConfiguration.getAuthenticationManager();
 //    }
 //}
-//package com.example.ecomm.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.example.ecomm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
